3db5df3fa3475751d4f5118c80efe618ae0affe9
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-10-11 14:22:01 -0700

    udpate models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,25 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+  
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
